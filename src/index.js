f1fece409cd061920c1c8d9cdb8a2207c107dba8
Feras Jobeir <fjobeir@gmail.com>
2022-06-27 13:50:02 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/css/bootstrap.css';
+import './assets/css/style.css';
+import './assets/css/theme.css';
+import './assets/css/fonts.css';
+import './assets/css/dark.css';
+import './assets/css/font-icons.css';
+import './assets/css/colors.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +15,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
